5f73a23ec60d6f6a9c6e60cf85dac84cc5b95346
kalyntn97 <kalyntn@gmail.com>
2023-09-01 15:19:25 -0400

    add page layout and buttons

--- js/app.js
+++ js/app.js
@@ -1,5 +1,11 @@
 import tetrominos from './data.js'
-const board = Array(20).fill(Array(10).fill(0))
- console.log(board)
- 
- console.log(tetrominos)
+const playField = Array(20).fill(Array(10).fill(0))
+
+const board = document.querySelector('.board')
+for (let i = 0; i < 10; i++) {
+  for (let j = 0; j < 20; j++) {
+    const cell = document.createElement('div')
+    cell.classList.add('cell')
+    board.appendChild(cell)
+  }
+}
